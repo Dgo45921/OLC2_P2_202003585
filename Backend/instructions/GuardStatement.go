329e601c72029eca0e6ebc6860afd6afaa9d5aff
Dgo45921 <3023135080101@ingenieria.usac.edu.gt>
2023-10-22 18:32:18 -0600

    guard statement added

--- Backend/instructions/GuardStatement.go
+++ Backend/instructions/GuardStatement.go
@@ -18,5 +18,50 @@ func NewGuard(lin int, col int, condition interfaces.Expression, falseb []interf
 }
 
 func (p Guard) Execute(ast *environment.AST, env interface{}, gen *generator.Generator) environment.Value {
-	return environment.Value{}
+	gen.AddComment("--- GUARD ---")
+	var condicion, result environment.Value
+	var newEnv = environment.NewEnvironment(env, environment.IF)
+	var OutLvls []interface{}
+	condicion = p.Condition.Execute(ast, env, gen) //imprime el if de operacion
+	retorno := gen.NewLabel()                      //salida
+	//*****************************************add true labels
+	for _, lvl := range condicion.TrueLabel {
+		gen.AddLabel(lvl.(string))
+	}
+	//instrucciones if
+	for _, inst := range p.FalseBlock {
+		result = inst.(interfaces.Instruction).Execute(ast, newEnv, gen)
+
+		//comprobar si es brak
+		if result.BreakFlag {
+			gen.AddGoto(gen.BreakLabel)
+			result.BreakFlag = false
+		} else if result.ContinueFlag {
+			gen.AddGoto(gen.ContinueLabel)
+			result.ContinueFlag = false
+		}
+
+		//out lbls
+
+		for _, lvl := range result.OutLabel {
+			OutLvls = append(OutLvls, lvl)
+		}
+
+	}
+
+	if !gen.Flag {
+		gen.AddGoto(retorno)
+	} else {
+		gen.AddGoto(gen.Auxlvl)
+	}
+
+	// adding false labels
+	for _, lvl := range condicion.FalseLabel {
+		gen.AddLabel(lvl.(string))
+	}
+
+	gen.AddLabel(retorno)
+	OutLvls = append(OutLvls, retorno)
+	result.OutLabel = DeepCopyArray(OutLvls).([]interface{})
+	return result
 }
