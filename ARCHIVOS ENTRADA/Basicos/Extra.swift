aa512513827c0f6b812ae6dffa895b1916d09adc
Dgo45921 <3023135080101@ingenieria.usac.edu.gt>
2023-10-26 19:57:16 -0600

    extra input file added

---
+++ ARCHIVOS ENTRADA/Basicos/Extra.swift
@@ -0,0 +1,20 @@
+ var prueba = 7
+ guard prueba % 2 == 0 else {
+        print("impar")
+        return
+    }
+
+var prueba2 = 5
+
+switch (prueba2){
+    case 1 :
+        print("soy un uno")
+    case 5 :
+        print ("soy un cinco")
+
+    default:
+        print("ni uno ni cinco")
+
+}
+
+
