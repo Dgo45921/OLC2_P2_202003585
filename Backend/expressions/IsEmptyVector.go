26a9ee20b35e8fccaef4942f8c6c93f9b4e14a50
Dgo45921 <3023135080101@ingenieria.usac.edu.gt>
2023-10-27 22:14:28 -0600

    isempty added

--- Backend/expressions/IsEmptyVector.go
+++ Backend/expressions/IsEmptyVector.go
@@ -17,6 +17,49 @@ func NewIsEmptyVector(lin int, col int, val string) IsEmptyVector {
 }
 
 func (p IsEmptyVector) Execute(ast *environment.AST, env interface{}, gen *generator.Generator) environment.Value {
+
 	var result environment.Value
+
+	VariableAccess{ID: p.Id}.Execute(ast, env, gen)
+
+	//llamada
+	newTmp := gen.NewTemp()
+	lvl1 := gen.NewLabel()
+	lvl2 := gen.NewLabel()
+	lvl3 := gen.NewLabel()
+
+	gen.AddGoto(lvl2)
+	gen.AddLabel(lvl1)
+	gen.AddPrintf("c", "66")
+	gen.AddPrintf("c", "111")
+	gen.AddPrintf("c", "117")
+	gen.AddPrintf("c", "110")
+	gen.AddPrintf("c", "100")
+	gen.AddPrintf("c", "115")
+	gen.AddPrintf("c", "69")
+	gen.AddPrintf("c", "114")
+	gen.AddPrintf("c", "114")
+	gen.AddPrintf("c", "111")
+	gen.AddPrintf("c", "114")
+	gen.AddGoto(lvl3)
+	gen.AddLabel(lvl2)
+
+	newTmp2 := gen.NewTemp()
+	gen.AddGetHeap(newTmp2, "(int)"+newTmp)
+	gen.AddLabel(lvl3)
+
+	tempo := gen.NewTemp()
+	gen.AddComment("isempty")
+
+	trueLabel := gen.NewLabel()
+	falseLabel := gen.NewLabel()
+
+	gen.AddIf(newTmp2, "0", "==", trueLabel)
+	gen.AddGoto(falseLabel)
+
+	result = environment.NewValue(tempo, true, environment.BOOLEAN)
+	result.TrueLabel = append(result.TrueLabel, trueLabel)
+	result.FalseLabel = append(result.FalseLabel, falseLabel)
+
 	return result
 }
